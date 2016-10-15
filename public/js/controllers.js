ae18a1be40d2ddd9fbc19e181d8c44a5b805a5a7
Sebastian Bejga <sebastian@bejga.de>
2016-10-15 19:35:04 +0200

    fix card image for "Pause" to be a cup

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -237,7 +237,7 @@ function ServerCtrl($scope, socket) {
 function AppCtrl($scope, socket, remember) {
 
     $scope.cardvalues = [
-        0, 0.5, 1, 2, 3, 5, 8, 13, 20, 40, 99, "PAUSE"
+        0, 0.5, 1, 2, 3, 5, 8, 13, 20, 40, 99, "Pause"
         //0, 0.5, 1, 2, 3, 5, 8, 13, 20, 40, 99
 		//0, 1, 2, 3, 4, 6, 8, 12, 16, 20, 24, 32, 40, 60
         //0, 0.5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 99
